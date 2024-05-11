883466d4f6f4ef2c42f0dae6fd04e907c8b281e9
satyam-seth <satyam1998.1998@gmail.com>
2024-05-11 17:40:57 +0530

    add upload endpoint

--- Full stack video streaming application/index.js
+++ Full stack video streaming application/index.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
-
+import path from "path"; // Add this line
+import fs from "fs";
+import { exec } from "child_process"; // watch out
 
 const app = express();
 
@@ -48,6 +50,39 @@ app.use("/uploads", express.static("uploads"));
 
 app.get('/', function(req, res) {
     res.json({message: "Hello Chai aur Code"});
+});
+
+app.post("/upload", upload.single('file'), function(req, res) {
+    const lessonId = uuidv4();
+    const videoPath = req.file.path;
+    const outputPath = `./uploads/courses/${lessonId}`;
+    const hlsPath = `${outputPath}/index.m3u8`;
+    console.log("hlsPath", hlsPath);
+
+    if (!fs.existsSync(outputPath)) {
+        fs.mkdirSync(outputPath, {recursive: true});
+    }
+
+    // ffmpeg
+    const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}`;
+
+    // no queue because of POC, not to be used in production
+    exec(ffmpegCommand, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`exec error: ${error}`)
+        }
+
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        
+        const videoUrl = `http://localhost:8000/uploads/courses/${lessonId}/index.m3u8`;
+
+        res.json({
+            message: "Video converted to HLS format",
+            videoUrl: videoUrl,
+            lessonId: lessonId,
+        });
+    });
 });
 
 
